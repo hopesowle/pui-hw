045665589dbd9317e571fa7ef0cf7a207329764d
hopesowle <143409146+hopesowle@users.noreply.github.com>
2023-09-30 19:18:53 -0400

    combine js

--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -51,3 +51,81 @@ rollPrice.innerText = rolls[rollType]["basePrice"];
 //pricecheck
 console.log(rolls[rollType]["basePrice"]);
 
+
+
+
+var basePrice = rollPrice;
+var gPrice = 0.00;
+var pPrice = 1.00;
+price.innerText = basePrice;
+
+let allGlazes = [
+    {
+        type: 'Keep original',
+        price: 0.00,
+    },
+    {
+        type: 'Sugar milk',
+        price: 0.00,
+    },
+    {
+        type: 'Vanilla milk',
+        price: 0.50,
+    },
+    {
+        type: 'Double chocolate',
+        price: 1.50,
+    },
+];
+
+let glazeSelect = document.getElementById("glaze-select");
+
+for (var i = 0; i < allGlazes.length; i++){
+    let gOption = document.createElement('option');
+    gOption.text = allGlazes[i].type;
+    gOption.value = allGlazes[i].price;
+    glazeSelect.add(gOption);
+}
+
+let allPacks = [
+    {
+        size: '1',
+        price: 1,
+    },
+    {
+        size: '3',
+        price: 3,
+    },
+    {
+        size: '6',
+        price: 5,
+    },
+    {
+        size: '12',
+        price: 10,
+    },
+];
+
+let packSelect = document.getElementById("pack-select");
+
+for (var j = 0; j < allPacks.length; j++){
+    let pOption = document.createElement('option');
+    pOption.text = allPacks[j].size;
+    pOption.value = allPacks[j].price;
+    packSelect.add(pOption);
+}
+
+function updatePrice() {
+    var finalPrice = (parseFloat(basePrice) + parseFloat(gPrice)) * parseInt(pPrice);
+    price.innerText = Math.round(100 * finalPrice) / 100;
+}
+
+function glazingChange(element) {
+    gPrice = element.value;
+    updatePrice();
+}
+
+function packChange(element) {
+    pPrice = element.value;
+    updatePrice();
+}
