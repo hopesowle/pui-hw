055dc595bdc234189d3b8d619078c61bcc309ce5
hopesowle <143409146+hopesowle@users.noreply.github.com>
2023-09-30 18:11:05 -0400

    move js

--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -24,3 +24,31 @@ const rolls = {
         "imageFile": "../assets/products/strawberry-cinnamon-roll.jpg"
     }    
 };
+
+const cart = {}
+
+//roll
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+
+//header
+const headerElement = document.querySelector('roll-header-text');
+headerElement.innerText = rollType;
+
+//headcheck
+console.log(rollType);
+
+//price
+const rollPrice = document.querySelector('roll-price');
+rollPrice.innerText = rolls.rollType.basePrice;
+
+//pricecheck
+console.log(rolls.rollType.basePrice);
+
+//img
+const rollImage = document.querySelector("#roll-img");
+rollImage.src = rolls.rollType.imageFile;
+
+//imgcheck
+console.log(rollImage.src = rolls.rollType.imageFile);
